a6c7d76f288222fe08a1741348300ddeae6d2f9a
stjesnay <jennifer.snay@stud.h-da.de>
2021-06-24 13:20:12 +0200

    Update uebung-animation.js

--- uebung-animation.js
+++ uebung-animation.js
@@ -1,21 +1,41 @@
 import gsap from "./gsap.min.js";
 
-const imdRechteck = { x: 10, y: 10 };
+const cube = { x: 10, y: 10, width: 50, height: 50 };
 
 function draw() {
   background(255);
-  rect(imdRechteck.x, imdRechteck.y, 50, 50);
+  rect(cube.x, cube.y, cube.width, cube.height);
 }
 
-gsap.to(imdRechteck, {
-  duration: 4,
-  ease: "linear",
-  x: 400,
-  onComplete: () => {
-    gsap.to(imdRechteck, {
-      duration: 4,
-      ease: "elastic",
-      y: 400,
-    });
-  },
-});
+function animation() {
+  gsap.to(cube, {
+    duration: 1,
+    ease: "easeOut",
+    x: 400,
+    onComplete: () => {
+      gsap.to(cube, {
+        duration: 0.5,
+        ease: "back",
+        y: 500,
+
+        onComplete: () => {
+          gsap.to(cube, {
+            duration: 2,
+            ease: "bounce",
+            y: 600,
+            width: 100,
+            height: 20,
+          });
+        },
+      });
+    },
+  });
+}
+
+function mouseClicked() {
+  cube.x = 10;
+  cube.y = 10;
+  cube.width = 50;
+  cube.height = 50;
+  animation();
+}
